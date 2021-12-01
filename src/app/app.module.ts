e368b44841576a8074147090cfd0aa4ab2eafd1f
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-01 12:52:24 +0100

    Dodanie Address component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
