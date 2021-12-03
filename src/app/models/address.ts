765f7ab77a5380fa94128c9a19608443e4bd74f4
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-03 20:17:11 +0100

    - dodanie klasy Address, która implementuje interfejs IAddress (zmieniona nazwa z Address)

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -1,4 +1,4 @@
-export interface Address {
+export interface IAddress {
   idAddress?: number;
   country?: string | null,
   region?: string | null,
@@ -6,4 +6,17 @@ export interface Address {
   street?: string | null,
   localNumber?: string | null,
   zipCode?: string | null
+}
+
+export class Address implements IAddress {
+  constructor(
+    public idAddress?: number,
+    public country?: string | null,
+    public region?: string | null,
+    public city?: string | null,
+    public street?: string | null,
+    public localNumber?: string | null,
+    public zipCode?: string | null
+  ) {
+  }
 }
