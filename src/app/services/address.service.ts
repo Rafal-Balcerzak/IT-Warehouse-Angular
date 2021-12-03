7e97e9860b3f8d3d7456441a9f9ee9da68d39176
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-03 20:18:39 +0100

    - podmiana zmienionej nazwy Address

--- src/app/services/address.service.ts
+++ src/app/services/address.service.ts
@@ -1,33 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Address} from "../models/address";
+import {IAddress} from "../models/address";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   /*** Pobranie wszystkich adresów ***/
-  getAllAddresses(): Observable<Array<Address>>{
-    return this.http.get<Array<Address>>('http://localhost:8080/api/address');
+  getAllAddresses(): Observable<Array<IAddress>> {
+    return this.http.get<Array<IAddress>>('http://localhost:8080/api/address');
   }
 
   /*** Dodanie nowego adresu ***/
-  addAddress(address: Address): Observable<Address>{
-    return this.http.post<Address>('http://localhost:8080/api/address/', address);
+  addAddress(address: IAddress): Observable<IAddress> {
+    return this.http.post<IAddress>('http://localhost:8080/api/address/', address);
   }
 
   /*** Usunięcia adresu o danym ID ***/
-  deleteAddressById(id: number){
+  deleteAddressById(id: number) {
     return this.http.delete('http://localhost:8080/api/address/' + id);
   }
 
   /*** Edytowanie adresu ***/
-  editAddress(address: Address): Observable<Address>{
-    return this.http.patch<Address>('http://localhost:8080/api/address/', address);
+  editAddress(address: IAddress): Observable<IAddress> {
+    return this.http.patch<IAddress>('http://localhost:8080/api/address/', address);
   }
 
 }
