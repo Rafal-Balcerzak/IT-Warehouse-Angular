f8573b607ed50e1b1cb89c7f905bf2893bf58461
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-03 20:30:46 +0100

    - utworzenie address-update.component.ts do edytowania i edycji adresów na bazie formularza

---
+++ src/app/address/address-update/address-update.component.ts
@@ -0,0 +1,130 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AddressService} from "../../services/address.service";
+import {NgbModal, NgbModalConfig} from "@ng-bootstrap/ng-bootstrap";
+import {Address, IAddress} from "../../models/address";
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'app-address-update',
+  templateUrl: './address-update.component.html',
+  styleUrls: ['./address-update.component.css']
+})
+export class AddressUpdateComponent implements OnInit {
+
+  @Input()
+  addressToEdit: Address;
+  @Output()
+  refreshList = new EventEmitter<boolean>();
+
+  editForm = this.fb.group({
+    idAddress: [],
+    country: [],
+    region: [],
+    city: [],
+    street: [],
+    localNumber: [],
+    zipCode: []
+  });
+
+  constructor(private addressService: AddressService,
+              config: NgbModalConfig,
+              private modalService: NgbModal,
+              protected fb: FormBuilder) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  /*** Otwiera okno zapisu/edycji i uzupełnia formularz w zależności od obecności adresu do edycji ***/
+  open(content) {
+    if (this.addressToEdit) {
+      this.updateForm();
+    }
+    this.modalService.open(content);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  /*** Zwraca nowy adres na bazie pól z formularza ***/
+  protected createFromForm(): IAddress {
+    return {
+      ...new Address(),
+      country: this.editForm.get(['country'])!.value,
+      region: this.editForm.get(['region'])!.value,
+      city: this.editForm.get(['city'])!.value,
+      street: this.editForm.get(['street'])!.value,
+      localNumber: this.editForm.get(['localNumber'])!.value,
+      zipCode: this.editForm.get(['zipCode']).value,
+    };
+  }
+
+  /*** Uzupełnia pola formularza jeśli ktoś edytuje ***/
+  protected updateForm(): void {
+    this.editForm.patchValue({
+      country: this.addressToEdit.country,
+      region: this.addressToEdit.region,
+      city: this.addressToEdit.city,
+      street: this.addressToEdit.street,
+      localNumber: this.addressToEdit.localNumber,
+      zipCode: this.addressToEdit.zipCode
+    })
+  }
+
+  /*** Czyśći formularz ***/
+  protected clearForm(): void {
+    this.editForm.patchValue({
+      idAddress: null,
+      country: null,
+      region: null,
+      city: null,
+      street: null,
+      localNumber: null,
+      zipCode: null
+    })
+  }
+
+  /*** Zapisuje lub edytuje adres, w zalezności od tego czy adreś do edycji został przekazany ***/
+  save(refreshList): void {
+    const address = this.createFromForm();
+    if (this.validateInput(address)) {
+      if (this.addressToEdit === null || this.addressToEdit === undefined) {
+        this.addressService.addAddress(address).subscribe(address => {
+          console.log("Dodano nowy adres: " + address);
+          this.refreshList.emit(refreshList);
+          this.clearForm();
+        });
+      } else {
+        address.idAddress = this.addressToEdit.idAddress;
+        this.addressService.editAddress(address).subscribe(address => {
+          console.log("Edytowano adres: " + address);
+          this.refreshList.emit(refreshList);
+          this.clearForm();
+        });
+      }
+    }
+  }
+
+  /*** Sprawdza czy wypełniane pola formularza nie są puste albo czy nie są spacjami itp ***/
+  validateInput(address: IAddress): boolean {
+    let addressFieldsList = [];
+
+    addressFieldsList.push(address.country);
+    addressFieldsList.push(address.region);
+    addressFieldsList.push(address.city);
+    addressFieldsList.push(address.street);
+    addressFieldsList.push(address.localNumber);
+    addressFieldsList.push(address.zipCode);
+
+    for (let i = 0; i < addressFieldsList.length; i++) {
+      let value = addressFieldsList[i];
+      if (value === '' || value === null || value === undefined || value.trim().length === 0) {
+        window.alert("Wypełnij wszystkie pola aby dodać adres");
+        console.log(value)
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
