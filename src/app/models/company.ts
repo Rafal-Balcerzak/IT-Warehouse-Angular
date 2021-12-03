2fa0243756994ee3f70401b4d6553e7e21eb15ff
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-03 21:12:39 +0100

    - dodanie klasy Company - zmiana nazwy interfejsu Company na ICompany

--- src/app/models/company.ts
+++ src/app/models/company.ts
@@ -1,8 +1,19 @@
 import {Address} from "./address";
 
-export interface Company {
+export interface ICompany {
   idCompany?: number;
   name?: string | null;
-  address: Address | null;
-  nip: string | null;
+  address?: Address | null;
+  nip?: string | null;
+}
+
+export class Company implements ICompany {
+  constructor(
+    public idCompany?: number,
+    public name?: string | null,
+    public address?: Address | null,
+    public nip?: string | null
+  ) {
+  }
+
 }
