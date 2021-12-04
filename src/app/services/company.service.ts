8c87687ebea36ecd3648a0112ec466ba589e38a7
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-04 14:27:49 +0100

    - dodanie zapytań do usuwania i edytowania firmy

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,22 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Company} from "../models/company";
+import {ICompany} from "../models/company";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   /*** Pobranie wszystkich firm ***/
-  getAllCompanies(): Observable<Array<Company>>{
-    return this.http.get<Array<Company>>('http://localhost:8080/api/company')
+  getAllCompanies(): Observable<Array<ICompany>> {
+    return this.http.get<Array<ICompany>>('http://localhost:8080/api/company')
   }
 
   /*** Dodanie nowej firmy ***/
-  addCompany(company: Company): Observable<Company>{
-    return this.http.post<Company>('http://localhost:8080/api/company/', company);
+  addCompany(company: ICompany): Observable<ICompany> {
+    return this.http.post<ICompany>('http://localhost:8080/api/company/', company);
+  }
+
+  /*** Usunięcie firmy o danycm ID ***/
+  deleteCompanyById(id: number) {
+    return this.http.delete('http://localhost:8080/api/company/' + id);
+  }
+
+  /*** Edytowanie firmy ***/
+  editCompany(company: ICompany): Observable<ICompany> {
+    return this.http.patch<ICompany>('http://localhost:8080/api/company/', company);
   }
 }
