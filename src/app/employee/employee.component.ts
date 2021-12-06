692e720dcffca23b4ea53790c517a81db0634004
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-06 21:52:38 +0100

    - dodanie widoku dla pracowników - wyświetlanie listy pracowników - usuwanie pracownika

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IEmployee} from "../models/employee";
+import {EmployeeService} from "../services/employee.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  allEmployees: Array<IEmployee> = []
+  showEmployeeList: boolean = false;
+  showEmployeeUpdate = true;
+
+  constructor(private employeeService: EmployeeService,
+              private modalService: NgbModal) {
+  }
 
   ngOnInit(): void {
+  }
+
+  //TODO dodać metody do otwierania okna edycji i dodawania nowych pracowników
+
+
+  /*** Pobranie wszystkich pracowników ***/
+  getAllEmployees(){
+    this.employeeService.getAllEmployees().subscribe(employee =>{
+      this.allEmployees = employee;
+      this.showEmployeeList = true;
+      console.log(employee);
+    }, error =>{
+      console.log("Błąd pobierania pracowników " + error);
+    })
+  }
+
+  /*** Usunięcie pracownika o danym ID ***/
+  deleteEmployeeById(id: number){
+    this.employeeService.deleteEmployeeById(id).subscribe(employee =>{
+      console.log("Usunięto pracownika: " + employee);
+      this.refreshList();
+    }, error => {
+      console.log("Błąd podczas usuwania pracownika: " + error);
+    })
+  }
+
+  /*** Wyczyszczenie tablicy ***/
+  clearAllEmployees(){
+    this.allEmployees = [];
+    this.showEmployeeList = false;
+  }
+
+  refreshList(){
+    this.getAllEmployees();
   }
 
 }
