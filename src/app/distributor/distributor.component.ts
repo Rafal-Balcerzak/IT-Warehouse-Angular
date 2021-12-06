b913b542d825d380163a4e01ca994dea26f079ac
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-06 14:50:59 +0100

    - dodanie pobierania listy dostawców

--- src/app/distributor/distributor.component.ts
+++ src/app/distributor/distributor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Distributor, IDistributor} from "../models/distributor";
+import {DistributorService} from "../services/distributor.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-distributor',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DistributorComponent implements OnInit {
 
-  constructor() { }
+  allDistributors: Array<IDistributor> = [];
+  showDistributorList: boolean = false;
+
+  constructor(private distributorService: DistributorService,
+              private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+
+  /*** Pobranie wszystkich dostawców ***/
+  getAllDistributors(){
+    this.distributorService.getAllDistributors().subscribe(distributor =>{
+      this.allDistributors = distributor;
+      this.showDistributorList = true;
+      console.log(distributor);
+    }, error => {
+      console.log("Błąd pobierania dostawców " + error);
+    })
+  }
+
+
 
 }
