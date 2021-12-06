225ddb120860e32857d02b631c37023d473574d0
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-06 14:50:04 +0100

    - dodanie routera dla Distributor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {AddressComponent} from "./address/address.component";
 import {CompanyComponent} from "./company/company.component";
 import {HomeComponent} from "./home/home.component";
 import {DemandComponent} from "./demand/demand.component";
+import {DistributorComponent} from "./distributor/distributor.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'address', component: AddressComponent},
   {path: 'company', component: CompanyComponent},
   {path: 'demand', component: DemandComponent},
+  {path: 'distributor', component: DistributorComponent},
 ];
 
 @NgModule({
