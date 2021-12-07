f24a1050abaa55d55f7724e4f7498aa3b9490960
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-07 18:34:57 +0100

    - dodanie moelu dla product

---
+++ src/app/models/product.ts
@@ -0,0 +1,32 @@
+import {ITransaction} from "./transaction";
+
+export interface IProduct {
+  idProduct?: number;
+  product_type?: string | null;
+  producer?: string | null;
+  model?: string | null;
+  inventoryNumber?: string | null;
+  price?: string | null;
+  productionDate?: Date | null;
+  warrantyEndDate?: Date | null;
+  warrantyType?: string | null;
+  inStock?: boolean | null;
+  transaction?: ITransaction | null;
+}
+
+export class Product implements IProduct {
+  constructor(
+    public idProduct?: number,
+    public product_type?: string | null,
+    public producer?: string | null,
+    public model?: string | null,
+    public inventoryNumber?: string | null,
+    public price?: string | null,
+    public productionDate?: Date | null,
+    public warrantyEndDate?: Date | null,
+    public warrantyType?: string | null,
+    public inStock?: boolean | null,
+    public transaction?: ITransaction | null
+  ) {
+  }
+}
