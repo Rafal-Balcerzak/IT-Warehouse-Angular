8a5dc447473585ac572dc48897f66617d6300194
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-07 10:52:59 +0100

    - dodanie modelu dla transakcji

---
+++ src/app/models/transaction.ts
@@ -0,0 +1,23 @@
+import {IDemand} from "./demand";
+import {IDistributor} from "./distributor";
+
+export interface ITransaction {
+  idTransaction?: number;
+  demand?: IDemand| null;
+  distributor?: IDistributor| null;
+  transactionDate?: Date| null;
+  price?: string| null;
+  description?: string| null;
+}
+
+export class Transaction implements ITransaction {
+  constructor(
+    public idTransaction?: number | null,
+    public demand?: IDemand | null,
+    public distributor?: IDistributor | null,
+    public transactionDate?: Date | null,
+    public price?: string | null,
+    public description?: string | null
+  ) {
+  }
+}
