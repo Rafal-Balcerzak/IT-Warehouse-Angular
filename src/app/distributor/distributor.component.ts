0d89a0f71f86eff763328d88cf4ccf2d508d2fe1
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-09 20:24:11 +0100

    - wyświetlanie dostawców w tabeli - dodanie paginacji dla dostawców - dodanie wyszukiwania dla dostawców

--- src/app/distributor/distributor.component.ts
+++ src/app/distributor/distributor.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {IDistributor} from "../models/distributor";
 import {DistributorService} from "../services/distributor.service";
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModal, NgbPaginationConfig} from "@ng-bootstrap/ng-bootstrap";
 import {DistributorUpdateComponent} from "./distributor-update/distributor-update.component";
 
 @Component({
@@ -14,12 +14,19 @@ export class DistributorComponent implements OnInit {
   allDistributors: Array<IDistributor> = [];
   showDistributorList: boolean = false;
   showDistributorUpdate = true;
+  page = 1;
+  pageSize = 5;
+  pageSizeList = [5, 10, 25, 50];
+  distributorsToShow: Array<IDistributor> = [];
 
   constructor(private distributorService: DistributorService,
-              private modalService: NgbModal) {
+              private modalService: NgbModal,
+              config: NgbPaginationConfig) {
+    config.boundaryLinks = true;
   }
 
   ngOnInit(): void {
+    this.getAllDistributors();
   }
 
   openAddDistributor() {
@@ -43,10 +50,26 @@ export class DistributorComponent implements OnInit {
     })
   }
 
+  /*** Wyszukiwanie po wpisanej frazie ***/
+  search(searchTerm: any) {
+    if (searchTerm !== null || true || searchTerm !== '') {
+      searchTerm = searchTerm.toLowerCase();
+    }
+    this.distributorsToShow = this.allDistributors.filter(distributor => {
+      if (distributor.idDistributor.toString().toLowerCase().indexOf(searchTerm) !== -1
+        || distributor.company.name.toLowerCase().indexOf(searchTerm) !== -1
+        || distributor.phoneNumber.toLowerCase().indexOf(searchTerm) !== -1
+        || distributor.email.toLowerCase().indexOf(searchTerm) !== -1) {
+        return distributor;
+      }
+    })
+  }
+
   /*** Pobranie wszystkich dostawców ***/
   getAllDistributors() {
     this.distributorService.getAllDistributors().subscribe(distributor => {
       this.allDistributors = distributor;
+      this.distributorsToShow = distributor;
       this.showDistributorList = true;
       console.log(distributor);
     }, error => {
